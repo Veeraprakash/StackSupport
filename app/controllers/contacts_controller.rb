185057e385db6df919f3733c8ae3ff3ab2d5c1b5
Veeraprakash <veeraprakasp@gmail.com>
2012-06-06 14:52:04 +0530

    commit19

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,30 +1,13 @@
 class ContactsController < ApplicationController
- def accinfo
-@acc = Contact.new    
 
-    respond_to do |format|
-      format.html 
-      format.xml  { render :xml => @acc }
-end
- end
-def acc_update
-   @acc=Contact.last
+ def acc_update
+   @acc=Contact.all
    @acc.update_attributes(params[:contact])
   redirect_to '/admin/home'
   end
-
-  def contact
-
- @contact = Contact.new    
-
-    respond_to do |format|
-      format.html 
-      format.xml  { render :xml => @contact }
-
-    end
-  end
+ 
 def contact_update
-   @contact=Contact.last
+   @contact=Contact.find(params[:id])
    @contact.update_attributes(params[:contact])
   redirect_to '/admin/home'
   end
